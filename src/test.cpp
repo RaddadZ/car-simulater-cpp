ab5f97b8eb65b3e79516b6b8fd8910435ec21b46
raddadz <ananraddad@gmail.com>
2016-05-20 23:00:17 +0000

    son duzelmeler, teslime hazir

--- src/test.cpp
+++ src/test.cpp
@@ -86,38 +86,6 @@ int main(){
 			default: cout << "Gecerli bir secenek numarisi girmediniz\n";
 		}
 	}
-
 	
-	// araba->benzinAl(20);
-
-	// cout << araba->depodakiYakit << " depodaki yakit\n";
-	// cout << araba->bilgisayar->getGenelKM() << " genel km \n";
-	// cout << araba->bilgisayar->getYerelKM() << " yerel km \n";
-	// cout << araba->bilgisayar->getOrtHiz() << " ort hiz \n";
-	// cout << araba->bilgisayar->getMenzil() << " menzil \n";
-	// cout << araba->bilgisayar->getOrtYakit()  << " ort yakit\n";
-	// cout << "done\n";
-
-	// araba->aracSur(90,200);
-
-	// cout << araba->depodakiYakit << " depodaki yakit\n";
-	// cout << araba->bilgisayar->getGenelKM() << " genel km \n";
-	// cout << araba->bilgisayar->getYerelKM() << " yerel km \n";
-	// cout << araba->bilgisayar->getOrtHiz() << " ort hiz \n";
-	// cout << araba->bilgisayar->getMenzil() << " menzil \n";
-	// cout << araba->bilgisayar->getOrtYakit() << " ort yakit\n";
-	// cout << "done\n";
-
-	// araba->benzinAl(20);
-	// araba->aracSur(200,80);
-
-	// cout << araba->depodakiYakit << " depodaki yakit\n";
-	// cout << araba->bilgisayar->getGenelKM() << " genel km \n";
-	// cout << araba->bilgisayar->getYerelKM() << " yerel km \n";
-	// cout << araba->bilgisayar->getOrtHiz() << " ort hiz \n";
-	// cout << araba->bilgisayar->getMenzil() << " menzil \n";
-	// cout << araba->bilgisayar->getOrtYakit() << " ort yakit\n";
-	// cout << "done\n";
-
 	return 0;
 }
